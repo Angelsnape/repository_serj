f93f7447f3692147cd9d8d6578a85d414879c5ad
angelsnape <sergey_23snp.ua@mail.ru>
2016-07-14 12:31:44 +0300

    save project

--- src/main/java/uits/jv1605kud/sergey/homework/task02/Task07.java
+++ src/main/java/uits/jv1605kud/sergey/homework/task02/Task07.java
@@ -12,7 +12,8 @@
  */
 package uits.jv1605kud.sergey.homework.task02;
 
-import java.util.Arrays;
+//import java.util.Arrays;
+
 
 /**
  *
@@ -22,28 +23,43 @@ public class Task07 {
     public static void main(String[] args) {
         int[][]array = new int[10][20];
          fill(array);
-    conclusion(horizontally(array, 7,"right"));
-        
+         conclusion(horizontally(array, 2,"right"));
+         
+         fillHor(array);
+         System.out.println();
+         conclusion(upright(array,3,"up"));
+       
+       
  
     }
     static void conclusion(int[][]array ){
-    for(int i = 0;i<array.length;i++){
-     for(int j = 0; j<array[i].length;j++){
-         System.out.print(array[i][j]+" ");
-     }
-        System.out.println();
-    }
+        for(int i = 0;i<array.length;i++){
+          for(int j = 0; j<array[i].length;j++){
+              System.out.print(array[i][j]+" ");
+            }
+         System.out.println();
+        
+        }
     }
     static void fill(int[][]array ){
-     for(int i = 0;i<array.length;i++){
-     for(int j = 0; j<array[i].length;j++){
-         array[i][j]= j;
-     }
-     
+      
+        for(int i = 0;i<array.length;i++){
+           for(int j = 0; j<array[i].length;j++){
+              array[i][j]= j;
+            }
+        }
      
     }
     
-    
+    static void fillHor(int[][]array){
+         int num = 0;
+            for(int i = 0;i<array.length;i++){
+               for(int j = 0; j<array[i].length;j++){
+                   array[i][j] = num;
+                }
+               
+              num++;
+            }
     
     }
     static int[][] horizontally(int[][] array, int how, String side){
@@ -73,7 +89,7 @@ public class Task07 {
              int a =0;
              
             for(int g = array[i].length-how;g<array[i].length;g++){
-                 array[i][g]=array_three[a];  
+                 array[i][g] = array_three[a];  
                     a++;
                 }
             }
@@ -109,9 +125,51 @@ public class Task07 {
         return array;
    }
     
-    static void upright(){
-    
-    
+    static int[][] upright(int[][]array, int how, String side){
+        int[][]array_two = new int[20][array.length];
+        
+           String up = "up";
+           String down = "down";
+           
+        if(up.equals(side)){
+              for(int i = 0; i<array.length;i++){
+                  for(int j = 0; j<array[i].length;j++){
+                     array_two[j][i]=array[i][j];
+         
+                    }
+                }
+          horizontally(array_two,how,"left");
+         
+              for(int i = 0; i<array.length;i++){
+                 for(int j = 0; j<array[i].length;j++){
+                    array[i][j]= array_two[j][i];
+         
+                  }
+                }
+        }
+        
+        
+        if(down.equals(side)){
+            
+              for(int i = 0; i<array.length;i++){
+                 for(int j = 0; j<array[i].length;j++){
+                    array_two[j][i]=array[i][j];
+         
+                  }
+                }
+              
+         horizontally(array_two,how,"right");
+         
+                for(int i = 0; i<array.length;i++){
+                  for(int j = 0; j<array[i].length;j++){
+                     array[i][j]= array_two[j][i];
+         
+                    }
+                }
+        }
+        
+        
+      return  array;
     }
 
   
